aec357245a9d79512207b5f836bc5e388552b2eb
technicise.com <info@technicise.com>
2013-04-13 22:00:27 +0530

    removing cntrols

--- code/desktop/ShoppingMallUI/ShoppingMallUI/Products.xaml.cs
+++ code/desktop/ShoppingMallUI/ShoppingMallUI/Products.xaml.cs
@@ -11,7 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using Microsoft.Windows.Controls;
+//using Microsoft.Windows.Controls;
 using ShoppingMallData;
 using System.Collections.ObjectModel;
 using ShoppingMallDb;
