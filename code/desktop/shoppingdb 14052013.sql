378e42d89cb9c62335847d8e39979f924c8a6171
Anirban Nandy <anirban.nandy@technicise.com>
2013-05-14 12:05:55 +0530

    db updated

---
+++ code/desktop/shoppingdb 14052013.sql
@@ -0,0 +1,110 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.9
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: May 14, 2013 at 06:34 AM
+-- Server version: 5.5.8
+-- PHP Version: 5.3.5
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `shoppingdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contactus`
+--
+
+CREATE TABLE IF NOT EXISTS `contactus` (
+  `id` varchar(100) NOT NULL,
+  `feedDate` datetime DEFAULT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `address` varchar(245) DEFAULT NULL,
+  `mobileno` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `feedback` varchar(545) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `contactus`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE IF NOT EXISTS `feedback` (
+  `id` varchar(100) NOT NULL,
+  `item` varchar(100) DEFAULT NULL,
+  `feedDate` datetime DEFAULT NULL,
+  `name` varchar(145) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  `rate` varchar(45) DEFAULT NULL,
+  `feedback` varchar(545) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `feedback`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+
+CREATE TABLE IF NOT EXISTS `product` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `brand` varchar(100) DEFAULT NULL,
+  `type` varchar(45) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `image` longblob,
+  `availableinshop` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `product`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `shop`
+--
+
+CREATE TABLE IF NOT EXISTS `shop` (
+  `id` varchar(100) NOT NULL,
+  `name` varchar(100) DEFAULT NULL,
+  `tag` varchar(100) DEFAULT NULL,
+  `type` varchar(100) DEFAULT NULL,
+  `availableinfloor` varchar(45) DEFAULT NULL,
+  `rating` varchar(45) DEFAULT NULL,
+  `description` varchar(500) DEFAULT NULL,
+  `availableProduct` varchar(500) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `shop`
+--
+
